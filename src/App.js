49da3e97bb1f6ee339e29b87ff7abdc94ffc2ffc
saikrishnachowdary <57797731+saikrishnachowdary@users.noreply.github.com>
2020-08-18 21:27:04 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+import useFetchJobs from './useFetchJobs'
+import { Container } from 'react-bootstrap'
+import Job from './Job'
+import JobsPagination from './JobsPagination';
+import SearchForm from './SearchForm';
+import { Card, Badge, Button, Collapse } from 'react-bootstrap';
+import './style.css'
+
+
+function App() {
+  const [params, setParams] = useState({})
+  const [page, setPage] = useState(1)
+  const { jobs, loading, error, hasNextPage } = useFetchJobs(params, page)
+
+  function handleParamChange(e) {
+    const param = e.target.name
+    const value = e.target.value
+    setPage(1)
+    setParams(prevParams => {
+      return { ...prevParams, [param]: value }
+    })
+  }
+
+  return (
+    <Container className="my-4">
+      <h2>Jobs Portal  by saikrishna pachala</h2>
+      <div>This poral jobs are from GitHub Api</div>
+
+    <Card className='cardc'>
+      <SearchForm params={params} onParamChange={handleParamChange} />
+    </Card>
+     
+      <JobsPagination page={page} setPage={setPage} hasNextPage={hasNextPage} />
+      {loading && <h1>Loading...</h1>}
+      {error && <h1>Error. Try Refreshing.</h1>}
+      {jobs.map(job => {
+        return <Job key={job.id} job={job} />
+      })}
+      <JobsPagination page={page} setPage={setPage} hasNextPage={hasNextPage} />
+    </Container>
+  )
+}
+
+export default App;
